7ac146024b8226f03485c8aabe25aea53ac15a2e
steve <gadafisteve001@gmail.com>
2025-08-16 11:01:10 +0300

    updates

--- AgriLink/urls.py
+++ AgriLink/urls.py
@@ -1,22 +1,39 @@
-"""
-URL configuration for AgriLink project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include  # include if you have other apps
+from django.urls import re_path
+from django.views.static import serve
+
+from django.conf.urls import handler404
+from django.conf.urls import handler500
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('farmers_app.urls')), 
 ]
+
+# Serve media and static files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+# This allows media files to be served even when DEBUG = False (for dev/testing)
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
+
+
+# from django.conf.urls import handler404, handler500
+
+# handler404 = 'farmers_website.views.custom_page_not_found'
+# handler500 = 'farmers_website.views.custom_server_error'
